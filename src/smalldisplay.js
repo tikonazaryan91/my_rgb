8ce2362e9c26958eada826f76104ac072f6da0b8
tikonazaryan91 <40820595+tikonazaryan91@users.noreply.github.com>
2018-12-11 23:47:15 +0400

    Add files via upload

---
+++ src/smalldisplay.js
@@ -0,0 +1,34 @@
+// import React from "react";
+//
+// function SmallDisplay(props){
+//     const {r, g, b} = props;
+//     // const color = `rgb(${r},${g},${b})`;
+//         return(
+//             <div>
+//                 {
+//                     <div className="small_dispalys"
+//                                style={{display: "flex", margin: 10}}>
+//                             <div style={{
+//                                 height: 50,
+//                                 width: 50,
+//                                 backgroundColor: `rgb(${r / 2},${g / 2},${b / 2})`,
+//                             }}/>
+//                             <div style={{
+//                                 height: 50,
+//                                 width: 50,
+//                                 backgroundColor: `rgb(${r / 1.8},${g / 1.8},${b / 1.8})`,
+//                             }}/>
+//                             <div style={{
+//                                 height: 50,
+//                                 width: 50,
+//                                 backgroundColor: `rgb(${r / 1.6},${g / 1.6},${b / 1.6})`,
+//                             }}/>
+//                         </div>
+//
+//                 }
+//             </div>
+//         );
+//
+// }
+//
+// export default SmallDisplay;
